6697e514b2092e79a7ec12daa14da8e8f4d16a94
fanxh <2295471337@qq.com>
2018-06-14 00:21:31 +0800

    !!!

--- xml解析/src/com/itheima/parse/XMLTest.java
+++ xml解析/src/com/itheima/parse/XMLTest.java
@@ -1,16 +1,21 @@
 package com.itheima.parse;
 
+import java.util.List;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
+import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 import org.junit.Test;
 
 public class XMLTest {
+	
 	@Test
 	public void xmlTest1() throws DocumentException {
 		SAXReader reader = new SAXReader();
 		Document document = reader.read("XML/fan.xml");
+		//dom4j½âÎö
 		Element rootElement = document.getRootElement();
 		System.out.println(rootElement);
 		Element pelement = rootElement.element("person");
@@ -20,5 +25,20 @@ public class XMLTest {
 		System.out.println(nelement.getText());
 		System.out.println(aelement.getText());
 		System.out.println(selement.getText());
+		
+		System.out.println("======Xpath½âÎö=========");
+
+	}
+	
+	@Test
+	public void xpathTest() throws DocumentException {
+		SAXReader reader = new SAXReader();
+		Document doc = reader.read("XML/fan.xml");
+		List<Node> list = doc.selectNodes("//name");
+		System.out.println(list);
+		for (Node node : list) {
+			Element e = (Element) node;
+			System.out.println(e.getText());
+		}
 	}
 }
