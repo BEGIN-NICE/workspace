26d43fa1a333ea3220d2848f29cc5de4d7c6ffa7
chaoliangFan <2295471337@qq.com>
2018-07-20 19:26:11 +0800

    struts2_02

--- struts2_day01/src/cn/heima/web/action/LoginAction.java
+++ struts2_day01/src/cn/heima/web/action/LoginAction.java
@@ -1,5 +1,25 @@
 package cn.heima.web.action;
 
 public class LoginAction {
-
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String login() {
+		if("tom".equals(this.username)&&"123".equals(this.password)) {
+			return "success";
+		}else {
+			return "failer";
+		}
+	}
 }
